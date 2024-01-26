d4c28d82003612a6e058e587d7ec030a44006ca0
Venkatesh <venkatesh.pandian@pinkerton.com>
2024-01-26 12:28:27 +0530

    Demo Implementation

--- app/src/main/java/com/venkatesh/imageupload/MainActivity.kt
+++ app/src/main/java/com/venkatesh/imageupload/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.venkatesh.imageupload
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.core.net.toFile
 import androidx.lifecycle.lifecycleScope
@@ -15,33 +19,43 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     private lateinit var imageUploadComponent: ImageUploadComponent
+    private var dialog: Dialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val builder: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this)
+        builder.setView(com.venkatesh.imageupload.R.layout.progress)
+        builder.setCancelable(false)
+        dialog = builder.create()
+
         imageUploadComponent = findViewById(R.id.uploadComponent)
 
         val repository = ImageRepositoryImpl(RetrofitClient.apiService)
+        var etName = findViewById<EditText>(R.id.et_name)
 
         imageUploadComponent.findViewById<Button>(com.venkatesh.imagepicker.R.id.submitButton).setOnClickListener {
             val selectedImageUri = imageUploadComponent.selectedImageUri
             lifecycleScope.launch {
                 if (selectedImageUri != null) {
-                    val file = selectedImageUri.toFile()
+                    val file = imageUploadComponent.convertImageUriToFile(selectedImageUri)
                     //optional
                     val compressedImage = imageUploadComponent.compressImage(file.path)
 
                     repository.uploadAttachments(compressedImage).collect {
                         when(it.status){
                             Status.LOADING->{
-                                Toast.makeText(this@MainActivity, "Loading..", Toast.LENGTH_SHORT).show()
+                                setDialog(true)
                             }
                             Status.SUCCESS->{
-                                Toast.makeText(this@MainActivity, "Success..", Toast.LENGTH_SHORT).show()
+                                setDialog(false)
+                                etName.setText(it.data?.location)
+                                Toast.makeText(this@MainActivity, "Image Uploaded Successfully", Toast.LENGTH_SHORT).show()
                             }
                             Status.ERROR->{
-                                Toast.makeText(this@MainActivity, "Error..", Toast.LENGTH_SHORT).show()
+                                setDialog(false)
+                                Toast.makeText(this@MainActivity, "Upload Error..", Toast.LENGTH_SHORT).show()
 
                             }
                         }
@@ -49,5 +63,9 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun setDialog(show: Boolean) {
+        if (show) dialog!!.show() else dialog!!.dismiss()
     }
 }
